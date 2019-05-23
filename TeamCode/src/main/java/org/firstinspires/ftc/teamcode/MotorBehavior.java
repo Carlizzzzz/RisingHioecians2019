27c1d7f6864911d146260727e3b8b843c9c3bcb1
Carlizzzzz <carlix200233@gmail.com>
2019-05-23 21:49:00 +0800

    motor be

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorBehavior.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorBehavior.java
@@ -8,18 +8,15 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 public class MotorBehavior {
-    private DcMotor controlMotor1, controlMotor2;
+    private DcMotor controlMotor;
     private int targetPosition,currentPosition,motorState;
     private double currentVelocity,currentError,lastError, intError;
     private double acc, dam, kp, ki;
 
-    public MotorBehavior(DcMotor motor1, DcMotor motor2, double _acc, double _dam, double _kp, double _ki) {
-        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        controlMotor1 = motor1;
-        //controlMotor2 = motor2;
-        controlMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        //controlMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    public MotorBehavior(DcMotor motor, double _acc, double _dam, double _kp, double _ki) {
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        controlMotor = motor;
+        controlMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         acc = _acc;
         dam = _dam;
         kp = _kp;
@@ -37,46 +34,35 @@ public class MotorBehavior {
     }
     public void update() {
         if (motorState == 1) {
-            controlMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            //controlMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            currentPosition = takeAverage();
+            controlMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            currentPosition = controlMotor.getCurrentPosition();
             lastError=currentError;
             intError += currentError;
             intError = Range.clip(intError, -100, 100);
-            currentError=takeAverage();
+            currentError=targetPosition-controlMotor.getCurrentPosition();
             if(targetPosition-currentPosition>0) {
                 currentVelocity = Math.sqrt((targetPosition - currentPosition) * acc * 2);
             }else{
                 currentVelocity = -Math.sqrt((currentPosition-targetPosition) * acc * 2);
             }
-            if(Math.abs(targetPosition-takeAverage())>200){
-                controlMotor1.setPower(Range.clip(currentVelocity, -0.7, 0.7));
-                //controlMotor2.setPower(Range.clip(currentVelocity, -0.7, 0.7));
+            if(Math.abs(targetPosition-controlMotor.getCurrentPosition())>200){
+                controlMotor.setPower(Range.clip(currentVelocity, -0.7, 0.7));
             }else{
                 double power = currentError*kp+(currentError-lastError)*dam+intError*ki;
-                controlMotor1.setPower(Range.clip(power, -0.7, 0.7));
-                //controlMotor2.setPower(Range.clip(power, -0.7, 0.7));
+                controlMotor.setPower(Range.clip(power, -0.7, 0.7));
             }
-            if(Math.abs(targetPosition-takeAverage())<15){
-                controlMotor1.setPower(0);
-                //controlMotor2.setPower(0);
+            if(Math.abs(targetPosition-controlMotor.getCurrentPosition())<15){
+                controlMotor.setPower(0);
                 //motorState=0;
             }
         }
         else if(motorState == 0){
 
         }else if(motorState==2){
-            controlMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            //controlMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            controlMotor1.setPower(0.2);
-            //controlMotor2.setPower(0.2);
-            controlMotor1.setTargetPosition(targetPosition);
-            //controlMotor2.setTargetPosition(targetPosition);
+            controlMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            controlMotor.setPower(0.2);
+            controlMotor.setTargetPosition(targetPosition);
         }
-    }
-
-    public int takeAverage(){
-        return (controlMotor1.getCurrentPosition()/*+controlMotor2.getCurrentPosition()*/)/2;
     }
 
 }
